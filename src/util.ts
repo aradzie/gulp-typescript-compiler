e27992d2823101b838a91727e3bdd93c0e196b84
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2015-11-09 14:09:58 +0300

    Fixed PassThroughStream

--- src/util.ts
+++ src/util.ts
@@ -18,40 +18,13 @@ export class PluginError extends _gu.PluginError {
     }
 }
 
-export class PassThroughStream extends _stream.Duplex {
-    private _files: _gu.File[] = [];
-    private _index = 0;
-
+export class PassThroughStream extends _stream.PassThrough {
     constructor(files?: _gu.File[]) {
         super({ objectMode: true });
         if (Array.isArray(files)) {
-            this._files = files.slice(0, files.length);
-        }
-    }
-
-    _write(file: _gu.File, encoding: string, cb: (error?) => void) {
-        if (!file) {
-            cb();
-            return;
-        }
-        if (file.isNull()) {
-            cb();
-            return;
-        }
-        if (file.isStream()) {
-            cb(new PluginError('Streaming not supported'));
-            return;
-        }
-        this.push(file);
-        cb();
-    }
-
-    _read() {
-        if (this._index < this._files.length) {
-            this.push(this._files[this._index++]);
-        }
-        else {
-            this.push(null);
+            for (let file of files) {
+                this.push(file);
+            }
         }
     }
 }
